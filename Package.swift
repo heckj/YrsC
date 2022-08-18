5dfe76ac618e844bc09566dbbe46e45dfd80cc07
Joe Heck <heckj@mac.com>
2022-08-18 13:35:01 -0700

    inc for release

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YrsC",
-            url: "https://github.com/heckj/YrsC/releases/download/0.11.2-alpha1/YrsC.xcframework.zip",
+            url: "https://github.com/heckj/YrsC/releases/download/0.11.2-alpha2/YrsC.xcframework.zip",
             checksum: "6ce0f000c6f177589a2a889b1db61e90ae194f1d528fdb8bd0389034d1236a6d"
         )
     ]
